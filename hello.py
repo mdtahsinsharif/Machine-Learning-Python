c2bf2165ce02bede76cb57fbe1720ceb68d3d05a
Md Tahsin Sharif <mt2shari@uwaterloo.ca>
2018-05-04 18:02:11 -0400

    Linear Regression for stocks : Practise

---
+++ hello.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import quandl
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import math
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+
+df = quandl.get("WIKI/GOOGL")
+df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+#print("Forecast value: " + (str)(forecast_out))
+#print("Forecast col: " + (str)(forecast_col))
+
+df.dropna(inplace=True)
+
+X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+#X = X[:-forecast_out]
+
+
+#X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+print(confidence)
+
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set, confidence, forecast_out)
+
+style.use('ggplot')
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+'''
+#X = X[:-forecast_out]
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+#clf = svm.SVR()
+
+#clf = LinearRegression()
+clf = LinearRegression(n_jobs=-1)
+
+clf.fit(X_train, y_train)
+
+confidence = clf.score(X_test, y_test)
+
+print(confidence)
+
+for k in ['linear','poly','rbf','sigmoid']:
+    clf = svm.SVR(kernel=k)
+    clf.fit(X_train, y_train)
+    confidence = clf.score(X_test, y_test)
+    print(k,confidence)
+
+print(df.head())
+'''
